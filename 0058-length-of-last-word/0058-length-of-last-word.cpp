b3d659afc8dbe5abddd7a8bc656c29a9e59a605d
Mohamed Hatem <mo.hatem1911@gmail.com>
2024-01-29 16:18:04 +0200

    Update 0058-length-of-last-word.cpp

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        reverse(s.begin(), s.end());
         int ans = 0;
-        bool found = 1;
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == ' ' && found) {
-                
-            } else if (s[i] == ' ' && !found) {
-                break;
-            } else {
-                found = 0;
-                ans++;
-            }
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s[i] == ' ') {
+                if(ans > 0) break;
+            } else ans++;
         }
         return ans;
     }
